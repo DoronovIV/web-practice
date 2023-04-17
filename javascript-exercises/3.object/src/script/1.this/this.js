8e93aa2af6c73cd56e36a9d562fdf5424d0485ec
DoronovIV <ipv2743@gmail.com>
2023-04-17 23:50:39 +0300

    Add calculator exrcsise

--- javascript-exercises/3.object/src/script/1.this/this.js
+++ javascript-exercises/3.object/src/script/1.this/this.js
@@ -38,3 +38,26 @@ export function makeChaining() {
       },
    };
 }
+
+export function makeNewCalculator() {
+   function calculate() {
+      this.read = function () {
+         this.firstValue = +prompt('Please specify the first value', 0);
+         this.secondValue = +prompt('Please specify the second value', 0);
+      };
+
+      this.sum = function () {
+         return this.firstValue + this.secondValue;
+      };
+
+      this.mul = function () {
+         return this.firstValue * this.secondValue;
+      };
+   }
+
+   let calculator = new Calculator();
+   calculator.read();
+
+   alert('Sum = ' + calculator.sum());
+   alert('Mul = ' + calculator.mul());
+}
