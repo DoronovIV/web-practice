c50870d34d486b68eab373ce67101ac77c465b3b
DoronovIV <ipv2743@gmail.com>
2023-05-04 20:46:50 +0300

    Find modal window bug

--- javascript-exercises/specific/0.shopping/src/script/controls/app.js
+++ javascript-exercises/specific/0.shopping/src/script/controls/app.js
@@ -6,5 +6,19 @@ export class App {
     run() {
         let ins = new ProductInserter();
         ins.insertAll();
+
+        let cart = document.querySelector('.page-content__header__content__cart-icon');
+        cart.addEventListener('mouseover', this._onCartMouseover);
+        cart.addEventListener('mouseleave', this._onCartMouseleave);
+    }
+
+    _onCartMouseover() {
+        const _cartWindow = document.querySelector('.page-content__header__cart-window');
+        _cartWindow.style.visibility = 'visible';
+    }
+
+    _onCartMouseleave() {
+        const _cartWindow = document.querySelector('.page-content__header__cart-window');
+        _cartWindow.style.visibility = 'hidden';
     }
 }
